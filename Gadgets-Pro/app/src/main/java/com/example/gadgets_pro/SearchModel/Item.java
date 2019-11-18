9d105f9b3e35aef989405e31cfeb69e65d6d59aa
Kevin Joseph <10507037@mydbs.ie>
2019-11-18 20:39:41 +0530

    SearchModel

---
+++ Gadgets-Pro/app/src/main/java/com/example/gadgets_pro/SearchModel/Item.java
@@ -0,0 +1,50 @@
+
+package com.myapp.ytvideos.SearchModel;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Item {
+
+    @SerializedName("kind")
+    @Expose
+    private String kind;
+    @SerializedName("etag")
+    @Expose
+    private String etag;
+
+    @SerializedName("snippet")
+    @Expose
+    private Snippet snippet;
+
+    @SerializedName("id")
+    @Expose
+    private Id id;
+
+
+
+    public String getEtag() {
+        return etag;
+    }
+
+    public void setEtag(String etag) {
+        this.etag = etag;
+    }
+
+    public Id getId() {
+        return id;
+    }
+
+    public void setId(Id id) {
+        this.id = id;
+    }
+
+    public Snippet getSnippet() {
+        return snippet;
+    }
+
+    public void setSnippet(Snippet snippet) {
+        this.snippet = snippet;
+    }
+
+}
